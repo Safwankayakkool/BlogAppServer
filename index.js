b3337920912dc340f4a2635098417e7a865221ff
Safwank734 <safwank734@gmail.com>
2021-07-08 15:22:05 +0530

    mongo added

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-  const express = require("express");
-  const mongoose = require("mongoose");
-  const port = process.env.MONGODB_URI || 5000;
-  const app = express();
-const uri="mongodb+srv://BlogApp:safwanchpu@cluster0.y4ubm.mongodb.net/myapp?retryWrites=true&w=majority";
-  mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-    });
+const express = require("express");
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
+const app = express();
 
-  const connection = mongoose.connection;
+mongoose.connect(
+  "mongodb+srv://BlogApp:safwanchpu@cluster0.y4ubm.mongodb.net/myapp?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
+const connection = mongoose.connection;
 
-  connection.once("open", () => {
-      console.log("Mongodb connected");
-  });
-  
-  app.use(express.json());
-  const userRoute = require("./routes/user");
-  app.use("/user",userRoute);
- 
+connection.once("open", () => {
+  console.log("Mongodb connected");
+});
 
-  app.route("/").get((req,res)=>res.json("your first rest api"));
+app.use(express.json());
+const userRoute = require("./routes/user");
+app.use("/user", userRoute);
 
-  app.listen(port, () => console.log(`your server started at port ${port}`));
+data = {
+  msg: "Welcome on DevStack Blog App development YouTube video series",
+  info: "This is a root endpoint",
+  Working: "Documentations of other endpoints will be release soon :)",
+  request:
+    "Hey if you did'nt subscribed my YouTube channle please subscribe it",
+};
+
+app.route("/").get((req, res) => res.json(data));
+
+app.listen(port, "0.0.0.0", () =>
+  console.log(`welcome your listinnig at port ${port}`)
+);
